9e22ff201b8d2109bdbb0ead424dcc171eccf24a
saykara <utkuanilsaykara@gmail.com>
2024-04-30 09:36:07 +0200

    Serialization process adapted

--- client/src/client.cpp
+++ client/src/client.cpp
@@ -3,6 +3,7 @@
 #include <thread>
 #include <cstring>
 #include <cstdint>
+#include <climits>
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
@@ -11,69 +12,83 @@
 
 #define PORT 8080
 #define BUFFER_SIZE 1024
+#define PACKET_SIZE sizeof(Message)
 #define FREQUENCY_MS 1000
 
-void udp_client() {
-    int sockfd;
-    struct sockaddr_in serv;
-    socklen_t addr_len = sizeof(serv);
-    char buffer[BUFFER_SIZE];
+void udp_client()
+{
+  int sockfd;
+  struct sockaddr_in serv;
+  socklen_t addr_len = sizeof(serv);
+  char buffer[PACKET_SIZE];
 
-    // Create UDP Socket file descriptor
-    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sockfd < 0) {
-        std::cerr << "Failed to create socket!" << std::endl;
-        return;
-    }
-
-    // Initializing server address
-    memset(&serv, 0, sizeof(serv));
-    serv.sin_family = AF_INET;
-    serv.sin_addr.s_addr = htonl(INADDR_ANY);
-    serv.sin_port = htons(PORT);
-
-    int ctr = 0;
-    std::string msg;
-    std::cout << "[UDP Client] running." << std::endl;
-    int seqNum = 0;
-
-    while (true) {
-        // Create request message
-        msg = std::to_string(ctr);
-
-        // Send request message to the server
-        ssize_t sendBytes = sendto(sockfd, msg.c_str(), msg.size(), 0, (struct sockaddr *)&serv, addr_len);
-        if (sendBytes < 0) {
-            std::cerr << "Failed to send message!" << std::endl;
-            break;
-        }
-
-        // Recieve response message from the server
-        ssize_t recvBytes = recvfrom(sockfd, buffer, BUFFER_SIZE, 0, nullptr, nullptr);
-        if (recvBytes < 0) {
-            std::cerr << "Failed while receiving message!" << std::endl;
-            break;
-        }
-
-        // Process received message
-        // TODO Check communication failures: repeated data, lost data, data out of order.
-        buffer[recvBytes] = '\0'; // Terminate the receiving buffer
-        std::cout << "Received from client: " << buffer << std::endl;
-
-        // Increment counter & wait for the specified frequency
-        if (ctr != UINT64_MAX) {
-            ctr++;
-        }
-        std::this_thread::sleep_for(std::chrono::milliseconds(FREQUENCY_MS));
-    }
-
-    close(sockfd);
+  // Create UDP Socket file descriptor
+  sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+  if (sockfd < 0)
+  {
+    std::cerr << "Failed to create socket!" << std::endl;
     return;
+  }
+
+  // Initializing server address
+  memset(&serv, 0, sizeof(serv));
+  serv.sin_family = AF_INET;
+  serv.sin_addr.s_addr = htonl(INADDR_ANY);
+  serv.sin_port = htons(PORT);
+
+  std::cout << "[UDP Client] running." << std::endl;
+  int seqNum = 0;
+  Message req, res;
+
+  while (true)
+  {
+    // Create request message
+    Message req = { seqNum, "Hello Alice!" };
+
+    // Serialize request message
+    char msg[PACKET_SIZE];
+    serializeMessage(req, msg);
+
+    // Send request message to the server
+    ssize_t sendBytes = sendto(sockfd, msg, PACKET_SIZE, 0, (struct sockaddr*)&serv, addr_len);
+    if (sendBytes < 0)
+    {
+      std::cerr << "Failed to send message!" << std::endl;
+      break;
+    }
+
+    // Recieve response message from the server
+    ssize_t recvBytes = recvfrom(sockfd, buffer, PACKET_SIZE, 0, nullptr, nullptr);
+    if (recvBytes < 0)
+    {
+      std::cerr << "Failed while receiving message!" << std::endl;
+      break;
+    }
+
+    // Process received message
+    req = deserializeMessage(buffer);
+    std::cout << "Received from server: " << req.seqNum << " - " << req.data << std::endl;
+
+    // Increment counter & wait for the specified frequency
+    if (seqNum != INT_MAX)
+    {
+      seqNum++;
+    }
+    else
+    {
+      seqNum = 0;
+    }
+    std::this_thread::sleep_for(std::chrono::milliseconds(FREQUENCY_MS));
+  }
+
+  close(sockfd);
+  return;
 }
 
-int main() {
-    // Run udp client
-    std::cout << "[UDP Client] starting." << std::endl;
-    udp_client();
-    return 0;
+int main()
+{
+  // Run udp client
+  std::cout << "[UDP Client] starting." << std::endl;
+  udp_client();
+  return 0;
 }
--- server/src/server.cpp
+++ server/src/server.cpp
@@ -7,67 +7,82 @@
 
 #include "utils.h"
 
-#define PORT 8080 
+#define PORT 8080
 #define BUFFER_SIZE 1024
+#define PACKET_SIZE sizeof(Message)
 
-void udp_server() {
-    int sockfd;
-    struct sockaddr_in serv, cli;
-    socklen_t addr_len = sizeof(cli);
-    char buffer[BUFFER_SIZE];
+void udp_server()
+{
+  std::cout << "Size of message is " << sizeof(Message) << std::endl;
+  int sockfd;
+  struct sockaddr_in serv, cli;
+  socklen_t addr_len = sizeof(cli);
+  char buffer[PACKET_SIZE];
 
-    // Create UDP Socket file descriptor
-    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sockfd < 0) {
-        std::cerr << "Failed to create socket!" << std::endl;
-        return;
-    }
-
-    // Initializing server address
-    memset(&serv, 0, sizeof(serv));
-    serv.sin_family = AF_INET;
-    serv.sin_addr.s_addr = htonl(INADDR_ANY);
-    serv.sin_port = htons(PORT);
-
-    // Bind socket address
-    if (bind(sockfd, (struct sockaddr *)&serv, sizeof(serv)) < 0) {
-        std::cerr << "Failed to bind socket address!" << std::endl;
-        return;
-    }
-    std::cout << "[UDP Server] running." << std::endl;
-
-    // Send & Receive
-    while (true) {
-        // Receive data from the client
-        ssize_t recvBytes = recvfrom(sockfd, &buffer, BUFFER_SIZE, 0, (struct sockaddr *)&cli, &addr_len);
-        if (recvBytes < 0) {
-            std::cerr << "Failed while receiving message!" << std::endl;
-            break;
-        }
-
-        // Process received message
-        // TODO Check communication failures: repeated data, lost data, data out of order.
-        buffer[recvBytes] = '\0'; // Terminate the receiving buffer
-        std::cout << "Received from client: " << buffer << std::endl;
-
-        // Respond to the client
-        const char* msg = "Message response!";
-        ssize_t sentBytes = sendto(sockfd, msg, strlen(msg), 0, (struct sockaddr *)&cli, addr_len);
-        if (sentBytes < 0) {
-            std::cerr << "Failed to send message!" << std::endl;
-            break;
-        }
-    }
-
-    // Close the socket
-    close(sockfd);
+  // Create UDP Socket file descriptor
+  sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+  if (sockfd < 0)
+  {
+    std::cerr << "Failed to create socket!" << std::endl;
     return;
+  }
+
+  // Initializing server address
+  memset(&serv, 0, sizeof(serv));
+  serv.sin_family = AF_INET;
+  serv.sin_addr.s_addr = htonl(INADDR_ANY);
+  serv.sin_port = htons(PORT);
+
+  // Bind socket address
+  if (bind(sockfd, (struct sockaddr*)&serv, sizeof(serv)) < 0)
+  {
+    std::cerr << "Failed to bind socket address!" << std::endl;
+    return;
+  }
+  std::cout << "[UDP Server] running." << std::endl;
+
+  int seqNum;
+  Message req, res;
+
+  // Send & Receive
+  while (true)
+  {
+    // Receive data from the client
+    ssize_t recvBytes = recvfrom(sockfd, &buffer, PACKET_SIZE, 0, (struct sockaddr*)&cli, &addr_len);
+    if (recvBytes < 0)
+    {
+      std::cerr << "Failed while receiving message!" << std::endl;
+      break;
+    }
+
+    // Process received message
+    req = deserializeMessage(buffer);
+    std::cout << "Received from client: " << req.seqNum << " - " << req.data << std::endl;
+
+    // Create response message
+    Message res{ req.seqNum, "Hello Bob!" };
+
+    // Serialize response message
+    char msg[PACKET_SIZE];
+    serializeMessage(res, msg);
+
+    ssize_t sentBytes = sendto(sockfd, msg, PACKET_SIZE, 0, (struct sockaddr*)&cli, addr_len);
+    if (sentBytes < 0)
+    {
+      std::cerr << "Failed to send message!" << std::endl;
+      break;
+    }
+  }
+
+  // Close the socket
+  close(sockfd);
+  return;
 }
 
-
-int main() {
-    // Run udp server
-    std::cout << "[UDP Server] starting." << std::endl;
-    udp_server();
-    return 0;
+int main()
+{
+  // Run udp server
+  std::cout << "[UDP Server] starting." << std::endl;
+  udp_server();
+  return 0;
 }
